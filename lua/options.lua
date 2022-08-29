51bfb8e04b960331933a5f9cf27b04e731509eb4
Joel Anttila <joel.anttila@wartsila.com>
2022-08-29 23:23:13 +0300

    User configured options

---
+++ lua/options.lua
@@ -0,0 +1,20 @@
+-- vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.mouse = "a"
+vim.opt.guifont = "FiraCode Nerd Font Mono:h11"
+
+vim.opt.termguicolors = true
+-- vim.opt.sessionoptions+=globals
