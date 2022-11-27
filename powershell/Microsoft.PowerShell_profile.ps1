71fa66bb1ded8960c0f4a5d60561a383b42fe5ba
Velho <jewel960@gmail.com>
2022-11-27 16:59:11 +0200

    Updating powershell profile.

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,55 @@
-# Set-PoshPrompt -Theme ys
 
-Import-Module -Name C:\Users\Velho\Documents\WindowsPowerShell\Scripts\Invoke-Environment.ps1
+# Get posh-theme by it's name and create path to it.
+# can be then 
+function PoshGetTheme($theme_name)
+{
+    # Make sure the selected theme exists.
+    $theme_folder = $env:POSH_THEMES_PATH
+    
+    $out = Get-ChildItem -Path $theme_folder `
+        | %{$_.FullName} | Select-String $theme_name
 
-Import-Module posh-git
-Import-Module PSReadLine
+    return $out
+}
 
-Set-PSReadLineKeyHandler -Key Tab -Function Complete
+# Initialize oh-my-posh.
+$posh_theme = PoshGetTheme("huvix")
+oh-my-posh init pwsh --config $posh_theme | Invoke-Expression
 
-oh-my-posh init pwsh --config ~/.ys.omp.json | Invoke-Expression
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Import-Module git-aliases -DisableNameChecking
+
+# Add alias for ll, usually goes as long-list (ls -l)
+# But everything in pwsh is long list,
+
+# Recursive list with depth=1.
+function ll {
+	Get-ChildItem -Recurse -Depth 1 $args
+}
+
+# Print out files.
+function la {
+	Get-ChildItem -File $args
+}
+
+# Print out path.
+function pyv {
+    echo "$args;$env:path"
+}
+
+# Append to $args to path.
+function add-path {
+    $env:path += ":$args"
+}
+
 
