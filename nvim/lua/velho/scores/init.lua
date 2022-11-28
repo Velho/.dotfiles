30565abb1552b631dee17d707b51cf5301562040
Velho <jewel960@gmail.com>
2022-11-28 02:13:35 +0200

    Moved files to scores folder.

--- nvim/lua/velho/scores/init.lua
+++ nvim/lua/velho/scores/init.lua
@@ -1,6 +1,6 @@
-print("apply fixes")
+print("apply scores")
 
 require'velho.fix.nvim-tree-autoclose'
 require'velho.fix.gitsigns-keymaps'
 require'velho.fix.nvim-treesitter-autogroups'
-
+require'velho.fix.telescope-in-folder'
