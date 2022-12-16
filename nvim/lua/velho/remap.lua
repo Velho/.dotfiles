e05b4240e270506ae911c4adc09c307b72bbb707
Velho <jewel960@gmail.com>
2022-12-16 19:32:43 +0200

    Renaming the remap.
    - Deleting the plugins-config, using after instead.

--- nvim/lua/velho/remap.lua
+++ nvim/lua/velho/remap.lua
@@ -10,10 +10,10 @@ map('n', '<leader>pv', ':Ex<CR>')
 map('n', '<leader>w', ':w<CR>')
 map('n', '<leader>l', ':ls<CR>')
 
-map('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>')
-map('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
-map('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
-map('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
+-- map('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>')
+-- map('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
+-- map('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
+-- map('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
 
 -- nvim-tree keymaps
 map('n', '<leader>tt', ':NvimTreeToggle<CR>')      -- tt -> TreeToggle
