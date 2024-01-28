ca7f64d4d795eddc39080e5b42294b0813c4a677
Velho <jewel960@gmail.com>
2024-01-28 17:09:20 +0200

    nvim: configuring hdl_checker lsp

--- nvim/after/plugin/lsp-confs.lua
+++ nvim/after/plugin/lsp-confs.lua
@@ -163,3 +163,38 @@ require('lspconfig').lua_ls.setup {
 
 require'lspconfig'.marksman.setup {}
 
+
+
+-- local val = os.execute('hdl_checker --version')
+-- print('ret : ', val)
+local f_hdl = io.popen('hdl_checker --version')
+local ver = f_hdl:read('*a')
+f_hdl:close()
+
+
+min_version = '^0.7'
+
+if ver ~= nil then
+
+  if ver:find(min_version) ~= nil then
+    print(string.format('hdl_checker: %s', ver))
+    -- Only define once
+    if not require'lspconfig.configs'.hdl_checker then
+        require'lspconfig.configs'.hdl_checker = {
+          default_config = {
+          cmd = {"hdl_checker", "--lsp", };
+          filetypes = {"vhdl", "verilog", "systemverilog"};
+            root_dir = function(fname)
+              -- will look for the .hdl_checker.config file in parent directory, a
+              -- .git directory, or else use the current directory, in that order.
+              local util = require'lspconfig'.util
+              return util.root_pattern('.hdl_checker.config')(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+            end;
+            settings = {};
+          };
+        }
+    end
+
+      require'lspconfig'.hdl_checker.setup{}
+  end
+end
