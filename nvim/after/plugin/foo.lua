0c29a76e6909c534b04a4e7a67651ff6938e446d
Velho <jewel960@gmail.com>
2024-04-11 14:08:30 +0300

    foo: Modifying todo-commenter regex to accept identifier
    Valid identifier can be anything from 2 to 6 characters.
    Examples are todo:, todo :, todo (id): todo(id):, todo (longer):

--- nvim/after/plugin/foo.lua
+++ nvim/after/plugin/foo.lua
@@ -15,10 +15,15 @@ require'todo-comments'.setup {
     before = "",
     keyword = "fg",
     after = "fg",
-    -- pattern = [[.*<(KEYWORDS|keywords)\s*:]] -- can you set the pattern directly accept lower-case?
+    --pattern = [[.*<(KEYWORDS|keywords)\s*:]] -- can you set the pattern directly accept lower-case?
+    -- matches the keyword upper or lowercase and identifier between parents
+    -- matching strings are todo: todo(id): todo (id): todo (longer):
+    pattern = [[.*<(KEYWORDS|keywords)\s*(\(\w{2,6}\))?\s*:]],
+    comments_only = true,
   }
 }
 
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
 require'ibl'.setup()
+
