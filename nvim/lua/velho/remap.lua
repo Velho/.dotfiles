a7798f63fad1d30167e8e19201f34e8a07e97cec
Velho <jewel960@gmail.com>
2024-07-01 15:45:24 +0300

    nvim: add keymaps to tabnext,prev

--- nvim/lua/velho/remap.lua
+++ nvim/lua/velho/remap.lua
@@ -15,6 +15,10 @@ map('n', '<leader>pv', ':Ex<CR>')
 map('n', '<leader>ll', ':ls<CR>')
 map('n', '<leader>w', ':w<CR>')
 
+map('n', '<leader>tp', ':tabp<CR>')
+map('n', '<leader>tn', ':tabn<CR>')
+
+
 -- map('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>')
 -- map('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
 -- map('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
