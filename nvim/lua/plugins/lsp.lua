e3f4f64aa636182e972066d2d82ecc5263b7189b
Velho <jewel960@gmail.com>
2026-04-12 21:54:53 +0300

    fix(nvim): Improving lsp configuration

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,66 +2,46 @@ return {
     {
         "neovim/nvim-lspconfig",
         dependencies = {
-            "stevearc/conform.nvim",
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-cmdline",
             "hrsh7th/nvim-cmp",
-            "L3MON4D3/LuaSnip",
             "saadparwaiz1/cmp_luasnip",
-            "j-hui/fidget.nvim",
         },
-        opts = {
-            servers = {
-                clangd = { mason = false },
-            },
-        },
+        event = "BufReadPre", 
         config = function()
+            local lspconfig = require("lspconfig")
             local cmp = require('cmp')
             local cmp_lsp = require("cmp_nvim_lsp")
-            local capabilities = vim.tbl_deep_extend(
-                "force",
-                {},
-                vim.lsp.protocol.make_client_capabilities(),
-                cmp_lsp.default_capabilities())
+            local capabilities = require("cmp_nvim_lsp").default_capabilities(
+                vim.lsp.protocol.make_client_capabilities()
+            )
 
-                require("fidget").setup({})
-                require("mason").setup()
-                require("mason-lspconfig").setup({
-                    ensure_installed = {
-                        "lua_ls",
-                    },
-                    handlers = {
-                        function(server_name) -- default handler (optional)
-                            require("lspconfig")[server_name].setup {
-                                capabilities = capabilities
-                            }
-                        end,
-                    }
-                })
+            local on_attach = function(client, bufnr)
+                local opts = { buffer = bufnr }
 
-                local cmp_select = { behavior = cmp.SelectBehavior.Select }
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+            end
 
-                cmp.setup({
-                    mapping = cmp.mapping.preset.insert({
-                        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                        ["<C-Space>"] = cmp.mapping.complete(),
-                    }),
-                    sources = cmp.config.sources({
-                        { name = 'nvim_lsp' },
-                    }, {
-                        { name = 'buffer' },
-                    })
-                })
+            local servers =  {
+                clangd = {},
+            }
 
-                vim.diagnostic.config({
-                    -- update_in_insert = true,
-                    float = {
+            for server, config in pairs(servers) do
+                vim.lsp.config[server] = {
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                    settings = config,
+                }
+                vim.lsp.enable(server)
+            end
+
+            vim.diagnostic.config({
+                -- update_in_insert = true,
+                float = {
                         focusable = false,
                         style = "minimal",
                         border = "rounded",
