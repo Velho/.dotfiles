fecdfd1dd2325ac30d5eee82144127e3da106624
Velho <jewel960@gmail.com>
2026-06-03 00:04:03 +0300

    feat(nvim,tree): Replace fancy neo-tree with old good nvim-tree

--- nvim/lua/velho/options.lua
+++ nvim/lua/velho/options.lua
@@ -54,3 +54,7 @@ vim.o.list = true
 
 vim.opt.background = "dark" -- set this to dark or light
 vim.cmd.colorscheme "oxocarbon"
+
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
